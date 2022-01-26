321ad404f1db60dde720c1a43ecfa737280bb030
Daniel Perez Jensen <danielperezjensen@gmail.com>
2022-01-26 14:39:25 +0100

    Moved processing code and added evaluation code

--- utils.py
+++ utils.py
@@ -1,17 +1,52 @@
-def generate_lags(df, value, n_lags):
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+def evaluate(model, validation_set, input_dim=None):
+    model.eval()
+    total_loss = 0.0
+
+    predictions = []
+    values = []
+
+    testloader = DataLoader(validation_set, batch_size=1, num_workers=2)
+
+    for i, data in enumerate(tqdm(testloader)):
+        inputs, targets = data
+        if input_dim:
+            inputs = inputs.view([1, -1, input_dim])
+
+        inputs = inputs.to(model.device)
+        targets = targets.to(model.device)
+
+        outputs = model(inputs)
+
+        predictions.append(outputs.detach().cpu().numpy())
+        values.append(targets.detach().cpu().numpy())
+
+    return predictions, values
+
+
+def format_predictions(predictions, values, scaler=None):
     """
-    generate_lags
-    Generates a dataframe with columns denoting lagged value up to n_lags
-    Args:
-        df: dataframe to lag
-        value: value to lag
-        n_lags: amount of rows to lag
+    Format predictions and values into dataframe for easy plotting
     """
-    df_n = df.copy()
+    vals = np.concatenate(values, axis=0).ravel()
+    preds = np.concatenate(predictions, axis=0).ravel()
 
-    for n in range(1, n_lags + 1):
-        df_n[f"lag_{n}"] = df_n[f"{value}"].shift(n)
+    df_result = pd.DataFrame(data={"value": vals, "prediction": preds})
 
-    df_n = df_n.iloc[n_lags:]
+    return df_result
 
-    return df_n
+
+def calculate_metrics(df):
+    """
+    Calculates various metrics on a df containing actual targets
+    and predicted targets
+    """
+    return {'mse': mean_squared_error(df.value, df.prediction),
+            'rmse': mean_squared_error(df.value, df.prediction) ** 0.5,
+            'r2': r2_score(df.value, df.prediction)}
