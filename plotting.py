7cfc2992ca4e183a67ccfc56cd8b1b90b7b9ba2a
Daniel Perez Jensen <danielperezjensen@gmail.com>
2022-01-26 14:40:53 +0100

    Added more args and moved data proc. to data.py

---
+++ plotting.py
@@ -0,0 +1,40 @@
+import plotly.graph_objs as go
+from plotly.offline import iplot
+import plotly.offline as pyo
+from torch.utils.data import DataLoader
+
+
+def plot_predictions(df_result):
+    data = []
+
+    value = go.Scatter(
+        x=df_result.index,
+        y=df_result.value,
+        mode="lines",
+        name="values",
+        marker=dict(),
+        text=df_result.index,
+        line=dict(color="rgba(0,0,0, 0.3)"),
+    )
+
+    data.append(value)
+
+    prediction = go.Scatter(
+        x=df_result.index,
+        y=df_result.prediction,
+        mode="lines",
+        line={"dash": "dot"},
+        name="predictions",
+        marker=dict(),
+        text=df_result.index,
+        opacity=0.8,
+    )
+
+    data.append(prediction)
+
+    layout = dict(
+        title="Predictions vs Actual Values for the dataset",
+    )
+
+    fig = dict(data=data, layout=layout)
+    iplot(fig)
--- train.py
+++ train.py
@@ -21,46 +21,38 @@ import argparse
 
 # Custom imports
 import models
-import datasets
+import data
 import utils
+import plotting
 
 
-def train(model_name):
+def train(model_name, time_features=False, plot=False):
 
-    processed_folder_path = os.path.join("data", "processed")
-
-    # Import river flow data and only preserve datapoints after 1965
-    df_DGA = pd.read_csv(os.path.join(processed_folder_path, "DGA.csv"),
-                         index_col=0, parse_dates=["date"])
-    df_DGA = df_DGA.loc[df_DGA["date"].dt.year >= 1965]
-
-    # Extract average monthly river flow
-    monthly_flow_data_mean = df_DGA.groupby(
-                                    pd.PeriodIndex(df_DGA['date'], freq="M")
-                                )['river_flow'].mean()
-    flow_mean_df = monthly_flow_data_mean.reset_index()
-
-    # Convert dataset to lagged dataset
-    df_generated = utils.generate_lags(flow_mean_df, "river_flow", 12)
-
-    dataset = datasets.RiverFlowDataset(df_generated)
+    dataset = data.gather_river_flow_data(lags=12, time_features=time_features)
     n = len(dataset)
 
-    train_set, val_set = torch.utils.data.random_split(dataset, [round(n * 0.8), round(n * 0.2)])
+    train_set, val_set = torch.utils.data.random_split(
+                            dataset, [round(n * 0.8), round(n * 0.2)]
+                        )
 
     batch_size = 1
+    loss_fn = nn.MSELoss(reduction="mean")
 
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False, num_workers=8)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=8)
+    train_loader = DataLoader(train_set, batch_size=batch_size,
+                              shuffle=False, num_workers=8)
+    val_loader = DataLoader(val_set, batch_size=batch_size,
+                            shuffle=False, num_workers=8)
 
     # Crude way to distinguish between models
     # TODO add parameters to parser
     if model_name == "MLP":
-        model = models.MLP(loss_fn=nn.MSELoss(reduction="mean"))
+        input_dim = len(train_set[0][0])
+
+        model = models.MLP(inputs=input_dim, loss_fn=loss_fn)
 
     elif model_name == "GRU":
 
-        input_dim = 12
+        input_dim = len(train_set[0][0])
         hidden_dim = 64
         output_dim = 1
         layer_dim = 3
@@ -69,18 +61,37 @@ def train(model_name):
         learning_rate = 1e-3
         weight_decay = 1e-6
 
-        model = models.GRU(input_dim, hidden_dim, layer_dim, output_dim, dropout_prob, lr=1e-3, loss_fn=nn.MSELoss(reduction="mean"))
+        model = models.GRU(input_dim, hidden_dim, layer_dim, output_dim,
+                           dropout_prob, lr=1e-3, loss_fn=loss_fn)
 
-    trainer = pl.Trainer(gpus=0, precision="bf16", max_epochs=100)
-    trainer.version = "GRU"
+    trainer = pl.Trainer(gpus=0, precision="bf16", max_epochs=5)
     trainer.fit(model, train_loader, val_loader)
+
+    # Evaluation after training
+    predictions, values = utils.evaluate(model, val_set)
+    df_results = utils.format_predictions(predictions, values)
+    results_metrics = utils.calculate_metrics(df_results)
+
+    print("Metrics of predicted values:")
+    for key, val in results_metrics.items():
+        print(f"{key.upper()}: {val:.3f}")
+
+    if plotting:
+        plotting.plot_predictions(df_results)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train model")
 
-    parser.add_argument("--model_name")
+    parser.add_argument("--model_name", default="MLP", type=str,
+                        help="Model to be trained")
+    parser.add_argument("--time_features", default=0, type=int,
+                        help="Include time as a (cyclical) feature")
+    parser.add_argument("--plot", default=0, type=int,
+                        help="Plot the predictions of the validation set")
 
     args = parser.parse_args()
 
-    train(args.model_name)
+    train(args.model_name,
+          time_features=args.time_features,
+          plot=args.plot)
