d8e436ac1782ccba0ea6bf82ebe318b20dc65ccc
Daniel Perez Jensen <danielperezjensen@gmail.com>
2022-06-29 16:39:50 +0200

    Minor refactor and added forgotten files

--- graph/models.py
+++ graph/models.py
@@ -21,7 +21,108 @@ ScalerType = Union[MinMaxScaler, StandardScaler,
                    MaxAbsScaler, RobustScaler, FunctionTransformer]
 
 
-class HeteroSeqLSTM(pl.LightningModule):
+class BaseModel(pl.LightningModule):
+    def __init__(self, cfg, metadata, scaler):
+        super().__init__()
+
+        self.cfg = cfg
+        self.metadata = metadata
+        self.scaler = scaler
+
+
+    def training_step(
+        self, batch: Batch, batch_idx: int
+    ) -> Dict[str, torch.Tensor]:
+        loss, output, target = self._shared_eval_step(batch, batch_idx)
+
+        self.log("train_loss", loss, on_epoch=True, on_step=True,
+                 batch_size=self.cfg.training.batch_size)
+
+        return {"loss": loss, "outputs": output, "targets": target}
+
+    def validation_step(
+        self, batch: Batch, batch_idx: int
+    ) -> Dict[str, torch.Tensor]:
+        loss, output, target = self._shared_eval_step(batch, batch_idx)
+
+        self.log("val_loss", loss, on_epoch=True, on_step=True,
+                 batch_size=self.cfg.training.batch_size)
+
+        return {"loss": loss, "outputs": output, "targets": target}
+
+    def test_step(
+        self, batch: Batch, batch_idx: int
+    ) -> Dict[str, torch.Tensor]:
+        loss, output, target = self._shared_eval_step(batch, batch_idx)
+
+        return {"loss": loss, "outputs": output, "targets": target}
+
+    def training_epoch_end(
+        self, training_step_outputs: List[Dict[str, torch.Tensor]]
+    ) -> None:
+
+        eval_dict = self._shared_eval_epoch(training_step_outputs)
+
+        for k in eval_dict.copy():
+            eval_dict[f"train_{k}"] = eval_dict.pop(k)
+
+        self.log_dict(eval_dict, on_epoch=True, prog_bar=True)
+
+    def validation_epoch_end(
+        self, validation_step_outputs
+    ) -> None:
+
+        eval_dict = self._shared_eval_epoch(validation_step_outputs)
+
+        for k in eval_dict.copy():
+            eval_dict[f"val_{k}"] = eval_dict.pop(k)
+
+        self.log_dict(eval_dict, on_epoch=True, prog_bar=True)
+
+    def test_epoch_end(
+        self, test_step_outputs: List[Dict[str, torch.Tensor]]
+    ) -> None:
+
+        eval_dict = self._shared_eval_epoch(test_step_outputs)
+
+        for k in eval_dict.copy():
+            eval_dict[f"test_{k}"] = eval_dict.pop(k)
+
+        self.log_dict(eval_dict, on_epoch=True)
+
+    def _shared_eval_step(
+        self, batch: Batch, batch_idx: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+
+        output = self(batch.xs_dict, batch.edge_indices_dict)
+        target = batch.y_dict["measurement"]
+
+        loss = self.loss_fn(output, target)
+
+        return loss, output, target
+
+    def _shared_eval_epoch(
+        self, step_outputs: List[Dict[str, torch.Tensor]]
+    ) -> Dict[str, float]:
+
+        outputs_list, targets_list = [], []
+
+        for out in step_outputs:
+            outputs_list.append(out["outputs"].reshape(-1, 4))
+            targets_list.append(out["targets"].reshape(-1, 4))
+
+        np_outputs = torch.cat(outputs_list).detach().cpu().numpy()
+        np_targets = torch.cat(targets_list).detach().cpu().numpy()
+
+        descaled_outputs = self.scaler.inverse_transform(np_outputs)
+        descaled_targets = self.scaler.inverse_transform(np_targets)
+
+        eval_dict = get_evaluation_measures(descaled_outputs, descaled_targets)
+
+        return eval_dict
+
+
+class HeteroSeqLSTM(BaseModel):
     def __init__(
         self,
         cfg: DictConfig,
@@ -29,7 +130,7 @@ class HeteroSeqLSTM(pl.LightningModule):
         scaler: ScalerType,
     ) -> None:
 
-        super().__init__()
+        super().__init__(cfg, metadata, scaler)
 
         self.cfg = cfg
         self.metadata = metadata
@@ -139,95 +240,124 @@ class HeteroSeqLSTM(pl.LightningModule):
 
         return self.optimizer
 
-    def training_step(
-        self, batch: Batch, batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
-        loss, output, target = self._shared_eval_step(batch, batch_idx)
 
-        self.log("train_loss", loss, on_epoch=True, on_step=True,
-                 batch_size=self.cfg.training.batch_size)
-
-        return {"loss": loss, "outputs": output, "targets": target}
-
-    def validation_step(
-        self, batch: Batch, batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
-        loss, output, target = self._shared_eval_step(batch, batch_idx)
-
-        self.log("val_loss", loss, on_epoch=True, on_step=True,
-                 batch_size=self.cfg.training.batch_size)
-
-        return {"loss": loss, "outputs": output, "targets": target}
-
-    def test_step(
-        self, batch: Batch, batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
-        loss, output, target = self._shared_eval_step(batch, batch_idx)
-
-        return {"loss": loss, "outputs": output, "targets": target}
-
-    def training_epoch_end(
-        self, training_step_outputs: List[Dict[str, torch.Tensor]]
+class HeteroSeqLSTM(BaseModel):
+    def __init__(
+        self,
+        cfg: DictConfig,
+        metadata: Tuple[List[str], List[Tuple[str, str, str]]],
+        scaler: ScalerType,
     ) -> None:
 
-        eval_dict = self._shared_eval_epoch(training_step_outputs)
+        super().__init__(cfg, metadata, scaler)
 
-        for k in eval_dict.copy():
-            eval_dict[f"train_{k}"] = eval_dict.pop(k)
+        self.cfg = cfg
+        self.metadata = metadata
+        self.scaler = scaler
 
-        self.log_dict(eval_dict, on_epoch=True, prog_bar=True)
-    def validation_epoch_end(
-        self, validation_step_outputs
-    ) -> None:
+        self.convs = nn.ModuleList()
 
-        eval_dict = self._shared_eval_epoch(validation_step_outputs)
+        # Convolutions dictated by config
+        if self.cfg.model.convolution.name == "sage":
+            for _ in range(cfg.model.num_layers):
+                conv = geom_nn.HeteroConv({
+                    edge_type: geom_nn.SAGEConv(
+                        in_channels=(-1, -1),
+                        out_channels=cfg.model.convolution.out_channels
+                    )
+                    for edge_type in metadata[1]
+                })
 
-        for k in eval_dict.copy():
-            eval_dict[f"val_{k}"] = eval_dict.pop(k)
+                self.convs.append(conv)
 
-        self.log_dict(eval_dict, on_epoch=True, prog_bar=True)
+        elif self.cfg.model.convolution.name == "gat":
+            for _ in range(cfg.model.num_layers):
+                conv = geom_nn.HeteroConv({
+                    edge_type: geom_nn.GATv2Conv(
+                        in_channels=(-1, -1),
+                        out_channels=cfg.model.convolution.out_channels,
+                        heads=cfg.model.convolution.heads,
+                        dropout=cfg.model.convolution.dropout_prob,
+                    )
+                    for edge_type in metadata[1]
+                })
 
-    def test_epoch_end(
-        self, test_step_outputs: List[Dict[str, torch.Tensor]]
-    ) -> None:
+                self.convs.append(conv)
 
-        eval_dict = self._shared_eval_epoch(test_step_outputs)
+        # Layer Norm for each node type
+        self.conv_normalizations = nn.ModuleDict(
+            {node: geom_nn.LayerNorm(cfg.model.convolution.out_channels)
+                for node in metadata[0]}
+        )
 
-        for k in eval_dict.copy():
-            eval_dict[f"test_{k}"] = eval_dict.pop(k)
+        # The lag is dictated by the data frequency
+        if cfg.data.freq == "M":
+            self.lag = 6
+        elif cfg.data.freq == "W":
+            self.lag = 24
 
-        self.log_dict(eval_dict, on_epoch=True)
+        self.gru = nn.GRU(
+            cfg.model.convolution.out_channels, cfg.model.hidden_dim,
+            batch_first=True
+        )
 
-    def _shared_eval_step(
-        self, batch: Batch, batch_idx: int
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # Layer norm for output of GRU
+        self.layer_norm = nn.LayerNorm(cfg.model.hidden_dim)
 
-        output = self(batch.xs_dict, batch.edge_indices_dict)
-        target = batch.y_dict["measurement"]
+        self.linear = nn.Linear(cfg.model.hidden_dim, 6)
 
-        loss = self.loss_fn(output, target)
+        self.loss_fn = nn.MSELoss()
 
-        return loss, output, target
+    def forward(
+        self, x_dict: TensorDict, edge_index_dict: TensorDict
+    ) -> torch.Tensor:
+        batch_size = x_dict["measurement"].size(0)
 
-    def _shared_eval_epoch(
-        self, step_outputs: List[Dict[str, torch.Tensor]]
-    ) -> Dict[str, float]:
+        # [B, T, N, D] -> [B x T x N, D]
+        for node_type in x_dict:
+            x_dict[node_type] = torch.flatten(x_dict[node_type], 0, 2)
 
-        outputs_list, targets_list = [], []
+        # Format edges to expected format of -> [2, M]
+        for edge_type in edge_index_dict:
+            edge_index_dict[edge_type] = edge_index_dict[
+                edge_type
+            ].permute((0, 2, 1)).flatten(0, 1).T
 
-        for out in step_outputs:
-            outputs_list.append(out["outputs"].reshape(-1, 4))
-            targets_list.append(out["targets"].reshape(-1, 4))
+        # Apply message passing
+        for conv in self.convs:
+            conv_out = conv(x_dict, edge_index_dict)
 
-        np_outputs = torch.cat(outputs_list).detach().cpu().numpy()
-        np_targets = torch.cat(targets_list).detach().cpu().numpy()
+        # Normalize output of message passing
+        for node_type, out in conv_out.items():
+            conv_out[node_type] = self.conv_normalizations[node_type](out)
 
-        descaled_outputs = self.scaler.inverse_transform(np_outputs)
-        descaled_targets = self.scaler.inverse_transform(np_targets)
+        # Take out measurement nodes and reshape to proper output
+        msr_out = conv_out["measurement"]
 
-        eval_dict = get_evaluation_measures(descaled_outputs, descaled_targets)
+        # Reshape to [batch_size, lag, n_stations, dimension]
+        msr_out = msr_out.reshape((batch_size, self.lag, 4, -1))
+        # Reshape to [batch_size, n_stations, lag, dimension]
+        msr_out = msr_out.permute((0, 2, 1, 3))
+        # Flatten into [batch_size * n_stations, lag, dimension]
+        msr_out = msr_out.flatten(0, 1)
 
-        return eval_dict
+        gru_out, _ = self.gru(msr_out)
+
+        # Normalize output of GRU
+        gru_out = self.layer_norm(gru_out)
+
+        gru_out = gru_out[:, -1, :]
+        gru_out = gru_out.reshape((batch_size, 4, -1))
+        lin_out = self.linear(gru_out)  # type: torch.Tensor
+
+        return lin_out
+
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        optimizer = get_optimizer(self.cfg.optimizer.name)
+        self.optimizer = optimizer(self.parameters(),
+                                   **self.cfg.optimizer.hparams)
+
+        return self.optimizer
 
 
 def get_evaluation_measures(
