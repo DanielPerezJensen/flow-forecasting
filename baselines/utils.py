Wrong order of return values in gather_preds

baselines/utils.py
@@ -55,7 +55,7 @@ def predict(
     predictions = np.array(predictions)
     values = np.array(values)
 
-    return predictions, values
+    return values, predictions
 
 
 def format_predictions(